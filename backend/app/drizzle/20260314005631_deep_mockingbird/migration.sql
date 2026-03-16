04530e1c22771573a31a0396c2971d26d621fe32
Zwykly <craftmor12@gmail.com>
2026-03-16 11:28:27 +0100

    Changed the project to use remoteDB, removed .env

--- backend/app/drizzle/20260314005631_deep_mockingbird/migration.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "classroom_reservations" ADD COLUMN "classroom_id" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "classroom_reservations" ADD COLUMN "online_classroom_id" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "classroom_reservations" ADD CONSTRAINT "classroom_reservations_classroom_id_classrooms_id_fkey" FOREIGN KEY ("classroom_id") REFERENCES "classrooms"("id");--> statement-breakpoint
-ALTER TABLE "classroom_reservations" ADD CONSTRAINT "classroom_reservations_rzJ1rOMLluAg_fkey" FOREIGN KEY ("online_classroom_id") REFERENCES "online_classrooms"("id");
--- backend/app/src/db/db.ts
+++ backend/app/src/db/db.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-// import { relations } from './schema';
 
 export const db = drizzle(process.env.DATABASE_URL!);

f1a125ebdab8da6fb008447016686daeaa767e5f
Zwykly <craftmor12@gmail.com>
2026-09-20 21:07:45 +0200

    fix(frontend): keep modal bottom sheet on landscape phones

--- frontend/src/components/common/Modal.tsx
+++ frontend/src/components/common/Modal.tsx
@@ -27,7 +27,7 @@ export function Modal({
                 <Dialog.Content
                     className={cn(
                         "fixed inset-x-0 bottom-0 z-50 mx-auto flex max-h-[90dvh] w-full max-w-2xl flex-col overflow-hidden rounded-t-2xl border border-light-grey bg-white p-4 pb-[max(1rem,env(safe-area-inset-bottom))] shadow-2xl focus:outline-none",
-                        "sm:inset-x-auto sm:bottom-auto sm:left-1/2 sm:top-1/2 sm:w-[92vw] sm:-translate-x-1/2 sm:-translate-y-1/2 sm:rounded-2xl sm:p-6",
+                        "lg:inset-x-auto lg:bottom-auto lg:left-1/2 lg:top-1/2 lg:w-[92vw] lg:-translate-x-1/2 lg:-translate-y-1/2 lg:rounded-2xl lg:p-6",
                         className,
                     )}
                 >
